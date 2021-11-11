c6c50ddb40c45f780e8d15f5bdc36a8273f5a050
ned1313 <ned.bellavance@gmail.com>
2021-11-11 16:39:57 -0500

    Change to the Free tier

--- main/variables.tf
+++ main/variables.tf
@@ -15,17 +15,17 @@ variable "naming_prefix" {
 variable "asp_tier" {
     type = string
     description = "Tier for App Service Plan (Standard, PremiumV2)"
-    default = "Standard"
+    default = "Free"
 }
 
 variable "asp_size" {
     type = string
     description = "Size for App Service Plan (S2, P1v2)"
-    default = "S2"
+    default = "F1"
 }
 
 variable "capacity" {
   type = string
   description = "Number of instances for App Service Plan"
-  default = "2"
+  default = "1"
 }
