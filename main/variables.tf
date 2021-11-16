230c9fa14cc4866452bb58598f124364d196dd84
ned1313 <ned.bellavance@gmail.com>
2021-11-16 12:21:57 -0500

    s1

--- main/variables.tf
+++ main/variables.tf
@@ -21,7 +21,7 @@ variable "asp_tier" {
 variable "asp_size" {
     type = string
     description = "Size for App Service Plan (S2, P1v2)"
-    default = "F1"
+    default = "S1"
 }
 
 variable "capacity" {
